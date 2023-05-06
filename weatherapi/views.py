cd1c8df5fa73b58d0943aa1da957bf289e43514b
Ashish Sharda <ashishjsharda@gmail.com>
2023-05-06 16:01:48 -0500

    Update views ,add env var

--- weatherapi/views.py
+++ weatherapi/views.py
@@ -1,1 +1,10 @@
 import requests
+import os
+from django.http import JsonResponse
+
+def get_weather(request):
+    api_key = os.environ.get('API_KEY')
+    city = request.GET.get('Austin')
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(url).json()
+    return JsonResponse(response)
